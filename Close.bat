54dde49776fc6b44803ac80fec7fa91c476d8622
Pascual Garcia <pascualgrc@gmail.com>
2025-12-17 19:21:38 -0800

    Initial Push to Master
    Contains the working files for the dummy prototype. Can be used as the main branch

---
+++ Close.bat
@@ -0,0 +1,5 @@
+taskkill /F /IM "DummyService1.exe" /T
+taskkill /F /IM "DummyService2.exe" /T
+taskkill /F /IM "DummyService3.exe" /T
+
+pause
---
+++ Launch.bat
@@ -0,0 +1,5 @@
+start C:\DummyServicePrototype\DummyPrototype\DummyService1\Debug\DummyService1.exe
+start C:\DummyServicePrototype\DummyPrototype\DummyService2\Debug\DummyService2.exe
+start C:\DummyServicePrototype\DummyPrototype\DummyService3\Debug\DummyService3.exe
+
+exit
